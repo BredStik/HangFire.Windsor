cb02e79f5f6aaad7b2329a8cff912ea47409f453
Mathieu Lafontaine <mathieulafontaine@hotmail.com>
2014-05-29 15:34:10 -0400

    Initial commit

---
+++ HangFire.Windsor.Tests/WindsorJobActivatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Castle.MicroKernel;
+using FakeItEasy;
+
+namespace HangFire.Windsor.Tests
+{
+    [TestClass]
+    public class WindsorJobActivatorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void New_activator_given_null_kernel_should_throw_ArgumentNullException()
+        {
+            var activator = new WindsorJobActivator(null);
+        }
+
+        [TestMethod]
+        public void New_activator_given_kernel_should_call_kernel_to_resolve_job_type()
+        {
+            var jobType = typeof(JobStub);
+            var actualJob = new JobStub();
+
+            var kernel = A.Fake<IKernel>();
+            A.CallTo(() => kernel.Resolve(jobType)).Returns(actualJob);
+
+            var activator = new WindsorJobActivator(kernel);
+
+            var returnedJob = activator.ActivateJob(jobType);
+
+            A.CallTo(() => kernel.Resolve(jobType)).MustHaveHappened();
+
+            Assert.AreEqual(actualJob, returnedJob);
+        }
+    }
+
+    public class JobStub
+    { 
+        
+    }
+}
---
+++ HangFire.Windsor/WindsorJobActivator.cs
@@ -0,0 +1,38 @@
+﻿using Castle.MicroKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangFire.Windsor
+{
+    /// <summary>
+    /// HangFire Job Activator based on Castle Windsor IoC Container.
+    /// </summary>
+    public class WindsorJobActivator: JobActivator
+    {
+        private readonly IKernel _kernel;
+
+        /// <summary>
+        /// Initializes new instance of WindsorJobActivator with a Windsor Kernel
+        /// </summary>
+        /// <param name="kernel">Kernel that will be used to create instance
+        /// of classes during job activation process.</param>
+        public WindsorJobActivator(IKernel kernel)
+        {
+            if (kernel == null) throw new ArgumentNullException("kernel");
+
+            _kernel = kernel;
+        }
+
+        /// <summary>
+        /// Activates a job of a given type using the Windsor Kernel
+        /// </summary>
+        /// <param name="jobType">Type of job to activate</param>
+        /// <returns></returns>
+        public override object ActivateJob(Type jobType)
+        {
+            return _kernel.Resolve(jobType);
+        }
+    }
+}
