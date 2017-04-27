cb9ddceef9438a2aa71761e5b883066d551d07b7
Jeroen Vervaeke <jeroen@vervaeke.nu>
2017-04-27 20:47:37 +0200

    Fixed the issue where resolved jobs aren't released

---
+++ HangFire.Windsor/WindsorJobActivatorScope.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.Windsor
+{
+    public class WindsorJobActivatorScope : JobActivatorScope
+    {
+        private readonly WindsorJobActivator _activator;
+        private readonly List<Object> _resolved = new List<Object>();
+
+        public WindsorJobActivatorScope(WindsorJobActivator activator)
+        {
+            if (activator == null) throw new ArgumentNullException(nameof(activator));
+            _activator = activator;
+        }
+
+        public override object Resolve(Type type)
+        {
+            var instance = _activator.ActivateJob(type);
+            _resolved.Add(instance);
+
+            return instance;
+        }
+
+        public override void DisposeScope()
+        {
+            foreach (var instance in _resolved)
+            {
+                (instance as IDisposable)?.Dispose();
+                _activator.ReleaseJob(instance);
+            }
+        }
+    }
+}
